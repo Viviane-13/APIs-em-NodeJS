b80d9e32e0abc4ad30d3216cf3b3e6352366f76e
Viviane <oviviane772@gmail.com>
2021-07-19 15:15:23 -0300

    List Reserve

--- DevHouse/src/controllers/ReserveController.js
+++ DevHouse/src/controllers/ReserveController.js
@@ -3,6 +3,14 @@ import User from '../models/User';
 import House from '../models/House';
 
 class ReserveController{
+  async index(req, res){
+    const {user_id} = req.headers;
+
+    const reserves = await Reserve.find({user: user_id}).populate('house');
+
+    return res.json(reserves);
+  }
+
   async store(req, res){
     const {user_id} = req.headers;
     const {house_id} = req.params;
@@ -33,6 +41,14 @@ class ReserveController{
 
     return res.json(reserve);
 
+  }
+
+  async destroy(req, res){
+    const {reserve_id} = req.body;
+
+    await Reserve.findByIdAndDelete({_id: reserve_id});
+
+    return res.send();
   }
 }
 
--- DevHouse/src/routes.js
+++ DevHouse/src/routes.js
@@ -20,6 +20,8 @@ routes.delete('/houses', HouseController.destroy);
 routes.get('/dashboard', DashboardController.show);
 
 routes.post('/houses/:house_id/reserve', ReserveController.store);
+routes.get('/reserves', ReserveController.index);
+routes.delete('/reserves/cancel', ReserveController.destroy);
 
 
 export default routes;
