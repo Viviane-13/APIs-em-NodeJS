76f8a44c9d301a94eaee02c9f020d50f2701dcfd
Viviane <oviviane772@gmail.com>
2021-07-23 14:28:52 -0300

    tasklist

--- tasklist/src/App.js
+++ tasklist/src/App.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 // Importando nossa database
@@ -13,6 +14,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
   }
 
