c3ff1454c8cf67568d8fcae6df4dd977a8491a06
Viviane <oviviane772@gmail.com>
2021-07-23 11:19:28 -0300

    Middleware de autenticação

--- tasklist/src/app/controllers/UserController.js
+++ tasklist/src/app/controllers/UserController.js
@@ -18,6 +18,11 @@ class UserController {
       email,
     });
   }
+
+  async update(req, res) {
+    console.log(req.userId);
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
--- tasklist/src/routes.js
+++ tasklist/src/routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
@@ -8,4 +10,8 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+// Todas as rotas abaixo desse middleware precisa estar autenticado
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 export default routes;
